267f739b6c56cc84a538f0a56a229eddae7b934a
chandro4D <chandroshakhor7@gmail.com>
2024-06-09 17:55:12 +0600

    add user and admin

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 7000;
@@ -31,7 +31,18 @@ async function run() {
     const productCollection = client.db("medicineDb").collection("products");
     const cartCollection = client.db("medicineDb").collection("carts");
     const userCollection = client.db("medicineDb").collection("users");
-    // ------------users related api--------
+    
+    
+    
+    // jwt related api------------
+    app.post('/jwt',async(req,res) => {
+      const user = req.body;
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET, {
+       expiresIn:'1h'
+      });
+      res.send({token});
+     })
+    // ------------users related api-------------------------------------------
     app.post('/users',async(req,res) => {
       const user = req.body;
       const query = {email: user.email}
@@ -56,6 +67,81 @@ async function run() {
     }
     const result = await userCollection.updateOne(filter,updatedDoc);
     res.send(result);
+  })
+
+  app.delete('/users/:id',async(req,res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await userCollection.deleteOne(query);
+    res.send(result);
+  })
+
+  // middlewares------------
+  const verifyToken = (req,res,next) => {
+    console.log('inside verify token',req.headers.authorization);
+    if(!req.headers.authorization){
+      return res.status(401).send({message:'unauthorize access'});
+    }
+    const token = req.headers.authorization.split(' ')[1]
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded) => {
+      if(err){
+        return res.status(401).send({message:'unauthorize access'});
+      }
+      req.decoded = decoded;
+      next();
+    })
+    
+   
+  }
+  // use verify admin after verifyToken
+  const verifyAdmin = async(req,res,next) => {
+    const email = req.decoded.email;
+    const query = {email: email};
+    const user = await userCollection.findOne(query);
+    const isAdmin = user?.role === 'admin';
+    if(!isAdmin){
+      return res.status(403).send({message: 'forbidden access'});
+    }
+    next()
+  }
+  // use verify seller after verifyToken
+  const verifySeller = async(req,res,next) => {
+    const email = req.decoded.email;
+    const query = {email: email};
+    const user = await userCollection.findOne(query);
+    const isSeller = user?.role === 'seller';
+    if(!isSeller){
+      return res.status(403).send({message: 'forbidden access'});
+    }
+    next()
+  }
+  app.get('/users/admin/:email',async(req,res) => {
+    const email = req.params.email;
+    // if(email !== req.decoded.email){
+    //   return res.status(403).send({message: 'forbidden access'})
+    // }
+    const query = {email: email};
+    const user = await userCollection.findOne(query);
+    let admin = false;
+    if(user){
+      admin = user?.role === 'admin';
+    }
+    res.send({admin});
+
+  })
+  app.get('/users/seller/:email',async(req,res) => {
+    const email = req.params.email;
+    // if(email !== req.decoded.email){
+    //   return res.status(403).send({message: 'forbidden access'})
+    // }
+    const query = {email: email};
+    const user = await userCollection.findOne(query);
+    let seller = false;
+    if(user){
+      seller = user?.role === 'seller';
+    }
+    res.send({seller});
+
   })
     // ------------get all shop products-----------------------
     app.delete('/carts/:id',async(req,res) => {
