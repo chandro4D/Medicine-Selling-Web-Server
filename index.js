846386479faa84ffe9bf98bfd0e738930e470527
chandro4D <chandroshakhor7@gmail.com>
2024-06-09 10:38:50 +0600

    add user dashboard

--- index.js
+++ index.js
@@ -42,6 +42,10 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
+    app.get('/users',async(req,res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+  })
     // ------------get all shop products-----------------------
     app.delete('/carts/:id',async(req,res) => {
       const id = req.params.id;
